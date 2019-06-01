6324de6e1305a518ffc87735e950c8e50098806d
johngraves <jg07024@gmail.com>
2019-06-01 16:40:15 +1200

    Dummy Profile with social media links

--- lib/section/profile_setion.dart
+++ lib/section/profile_setion.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfileSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text("Profile section"),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Your Profile\n',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
+          Text(
+            'John Doe\nCoding Rock Star\n',
+            style: TextStyle(fontSize: 16.0),
+          ),
+          InkWell(
+            child: Text("LinkedIn\n",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.blue)),
+            onTap: () {
+              _launchURL('https://www.linkedin.com/');
+            },
+          ),
+          InkWell(
+            child: Text("Twitter",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.blue)),
+            onTap: () {
+              _launchURL('https://twitter.com/');
+            },
+          ),
+        ],
+      ),
     );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
