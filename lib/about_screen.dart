5199a13ac556a4b464e3d8453d9905bb2e9fccfc
johngraves <jg07024@gmail.com>
2019-06-01 14:17:37 +1200

    Added full text for About Screen

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -10,17 +10,31 @@ class AboutScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
-              'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. ',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              'Welcome!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
             ),
             Text(
-              'Built by:',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              'Inspired by a Google Docs spreadsheet and a prize-winning Shiny app, this app helps the Flutter community to share links to Flutter-related resources.\n\nUse the app to browse to the resources you need most: books, libraries, tutorials, Meetup groups, videos.\n',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              'Contribute',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+            ),
+            Text(
+              'Found something great you want to share? Great! First check to see if it is already listed. If not, add it to the appropriate category.\n\nWe appreciate it!\n',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Text(
+              'Built by Got the Flutters for Hack19',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
             ),
             Text(
               'Fraser Croad, Linus WK Chan, John Graves and Nic Tolentino',
+              style: TextStyle(fontSize: 16.0),
             ),
           ],
         ),
